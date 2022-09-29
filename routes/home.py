eadc9f4b1917ce56b7b0e34abc30e3d92867d903
daridev <hernandezdarifrancisco@gmail.com>
2022-09-29 10:09:39 -0500

    search form layout

--- routes/home.py
+++ routes/home.py
@@ -6,3 +6,7 @@ blueprint_home = Blueprint ('home', __name__)
 @blueprint_home.route ("/")
 def index ():
     return render_template ("index.html", current_page="home")
+
+@blueprint_home.route ("/search/")
+def search_all ():
+    return render_template ("search.html", current_page="all")
